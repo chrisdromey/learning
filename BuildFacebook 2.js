49c80121d1d4b8d7033861b0bbe3864e096c52b0
Chris Dromey <Chris.dromey@bbc.co.uk>
2020-06-24 09:15:42 +0100

    Fixed
    Changed the indenting slightly which made it easier to spot the missing } from the `signIn` function.

--- BuildFacebook 2.js
+++ BuildFacebook 2.js
@@ -1,38 +1,41 @@
 //create the data//
 
-var database = [ {
-    username: "Lucy",
-    password: "IloveJS",
-},{
-    username: "Carmen",
-    password: "neighbour",
-}, {
-    username: "Ingrid",
-    password: "neigh",
-
-}]
+var database = [
+    {
+        username: "Lucy",
+        password: "IloveJS",
+    },
+    {
+        username: "Carmen",
+        password: "neighbour",
+    },
+    {
+        username: "Ingrid",
+        password: "neigh",
+    }
+];
 
 var newsFeed = [
     {
         username: "Vee",
         timeline: "I love Lucy",
-    },{
+    },
+    {
         username: "Count Duckula",
         timeline: "Quack",
-    }
-
+    } 
 ];
 
 function signIn(username, password) {
-    if (username === database[0].username 
-        && password === database[0].password) {
-            console.log(newsFeed);  
-        } else {
-    alert("Sorry wrong username and password");
+    if (username === database[0].username && password === database[0].password) {
+        console.log(newsFeed);  
+    } else {
+        alert("Sorry wrong username and password");
+    }
 }
 
 userNamePrompt = prompt("What is your username?");
 passwordPrompt = prompt("What is your password?");
 
 //now we must call the function - the parameters must match the arguments that the signIn function is wanting//
-signIn(userNamePrompt, passwordPrompt)
+signIn(userNamePrompt, passwordPrompt);
